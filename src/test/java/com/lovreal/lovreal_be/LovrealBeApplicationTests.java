3bbfa27a482142711ffc11736096145d9d897ac9
hyo0joo <hyoeun.jin2@gmail.com>
2025-07-21 02:12:33 +0900

    초기 커밋

---
+++ src/test/java/com/lovreal/lovreal_be/LovrealBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovreal.lovreal_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovrealBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
